242087ac53a6a2aaf0134a65cc1498ead5891ece
Dominik <dominikb17106@gmail.com>
2017-07-09 22:58:02 +0200

    Initial commit

---
+++ site-core/src/main/java/pl/site/SiteCoreApplication.java
@@ -0,0 +1,12 @@
+package pl.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCoreApplication.class, args);
+	}
+}
---
+++ site-db/src/main/java/pl/site/SiteDbApplication.java
@@ -0,0 +1,12 @@
+package pl.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteDbApplication.class, args);
+	}
+}
